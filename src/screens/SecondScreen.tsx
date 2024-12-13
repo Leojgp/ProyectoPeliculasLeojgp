8af936a06322db2ffd8c35b1299756eb86e9746a
Leojgp <leonardoljgp@gmail.com>
2024-12-13 18:54:26 +0100

    Cambiando Fondo de la segunda screen de configuración

--- src/screens/SecondScreen.tsx
+++ src/screens/SecondScreen.tsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Slider from '../components/Slider'
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import Slider from '../components/Slider';
 import { useMovies } from '../hooks/useMovies';
+import { useTheme } from '../context/ThemeContext';
 
 export default function SettingsScreen() {
   const { nowPlaying, loading, setPage } = useMovies();
+
+  const { backgroundColor, setBackgroundColor } = useTheme();
+
+  useEffect(() => {
+    // Cambio el fondo a azul oscuro
+    setBackgroundColor('darkblue');
+  }, [setBackgroundColor]);
 
   const loadMoreMovies = () => {
     if (nowPlaying && !loading) {
@@ -12,18 +20,34 @@ export default function SettingsScreen() {
       setPage((prevPage) => prevPage + 1);
     }
   };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text style={styles.header}>Configuraciones</Text>
       <Slider
         movies={nowPlaying?.movies}
-        height={600}
-        backgroundColor="black"
+        height={400}
+        backgroundColor="lightblue"
         // Paso la función para cargar más películas que he definido arriba
         onEndReached={loadMoreMovies}
       />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: '#ffdd57',
+    textAlign: 'center',
+    marginBottom: 20,
+    textShadowColor: 'rgba(0, 0, 0, 0.7)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 8,
+  },
+});
