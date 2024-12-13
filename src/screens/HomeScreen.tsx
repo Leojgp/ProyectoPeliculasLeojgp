a383572372f04026de96c680f6b23b08e25fe51f
Leojgp <leonardoljgp@gmail.com>
2024-12-13 14:35:43 +0100

    Eliminando el Botón y haceindo que el paso de páginas se haga a través del slider

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,28 +1,30 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useMovies } from '../hooks/useMovies'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useMovies } from '../hooks/useMovies';
 import Slider from '../components/Slider';
 
 export default function HomeScreen() {
-    const { nowPlaying, loading, setNowPlaying} = useMovies();
+    const { nowPlaying, loading, setPage } = useMovies();
+
+    const loadMoreMovies = () => {
+        if (nowPlaying && !loading) { 
+            // Incremento la página en 1
+            setPage((prevPage) => prevPage + 1); 
+        }
+    };
+
     return (
         <View>
             <Text>HomeScreen</Text>
-            <Slider movies={nowPlaying?.movies} height={300} backgroundColor="lightyellow"/>
-            <Button
-            title='Siguiente'
-            onPress={()=>{
-                if (nowPlaying) {
-                    const newNowPlaying = {
-                        ...nowPlaying,
-                        page: nowPlaying.page + 1,
-                    };
-                    setNowPlaying(newNowPlaying);
-                }
-            }}
+            <Slider
+                movies={nowPlaying?.movies}
+                height={300}
+                backgroundColor="green"
+                // Paso la función para cargar más películas que he definido arriba
+                onEndReached={loadMoreMovies} 
             />
         </View>
-    )
+    );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
